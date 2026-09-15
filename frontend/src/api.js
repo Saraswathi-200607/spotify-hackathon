70de1de697ac7eafc78a3b79e7a01a6de7fbf54d
Neelakantam <saraswathigovardhan7@gmail.com>
2026-09-15 19:14:55 +0530

    api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,17 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080"
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-
-  return config;
-});
+);
 
 export default api;
